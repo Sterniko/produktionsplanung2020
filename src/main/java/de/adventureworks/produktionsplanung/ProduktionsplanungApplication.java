02b5aeea9dedbaaa2a9895bae22a2e298816e6a2
robertkossendey <Gebaum11>
2019-01-23 14:19:52 +0100

    mockup link hinzugefügt (noch in arbeit)

---
+++ src/main/java/de/adventureworks/produktionsplanung/ProduktionsplanungApplication.java
@@ -0,0 +1,14 @@
+package de.adventureworks.produktionsplanung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduktionsplanungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduktionsplanungApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/de/adventureworks/produktionsplanung/ProduktionsplanungApplicationTests.java
@@ -0,0 +1,17 @@
+package de.adventureworks.produktionsplanung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProduktionsplanungApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
