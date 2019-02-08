18724e4d26e8f0837d4d176b5526c4604314c32d
johannes <johannes.kotsch@haw-hamburg.de>
2019-02-08 14:48:14 +0100

    first warehouse controller

---
+++ src/main/java/de/adventureworks/produktionsplanung/controller/WarehouseController.java
@@ -0,0 +1,38 @@
+package de.adventureworks.produktionsplanung.controller;
+
+import de.adventureworks.produktionsplanung.model.DataBean;
+import de.adventureworks.produktionsplanung.model.entities.bike.Component;
+import de.adventureworks.produktionsplanung.model.entities.businessPeriods.BusinessDay;
+import de.adventureworks.produktionsplanung.model.entities.businessPeriods.BusinessWeek;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public class WarehouseController {
+
+    private DataBean dataBean;
+
+    public WarehouseController(DataBean dataBean) {
+        this.dataBean = dataBean;
+    }
+
+    @RequestMapping("/warehouse")
+    public String getBusinessWeeks(Model model) {
+        model.addAttribute("businessWeeks", dataBean.getBusinessWeeks())
+        return "warehouse";
+    }
+
+    public void postComponentChange(LocalDate date, Map<Component, Integer> newStock) {
+        BusinessDay businessDay =;
+        //TODO: businessDay of LocalDate
+
+        businessDay.setWarehouseStok(newStock);
+
+
+
+    }
+
+}
