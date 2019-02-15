3c4cc894224814f9e8bda846ca1c82054da216b4
robertkossendey <Gebaum11>
2019-02-15 20:57:36 +0100

    masterData weitergemacht

--- src/main/java/de/adventureworks/produktionsplanung/controller/masterData/MasterDataController.java
+++ src/main/java/de/adventureworks/produktionsplanung/controller/masterData/MasterDataController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MasterDataController {
@@ -20,7 +21,22 @@ public class MasterDataController {
     public String getMasterData(Model model) {
         model.addAttribute("hourlyCapacity", dataBean.getHourlyCapacity());
         model.addAttribute("yearlyProduction", dataBean.getYearlyProduction());
+        model.addAttribute("masterDataRequest", new MasterDataRequest());
+
         return "masterData";
+    }
+
+    @RequestMapping(value = "/masterData", method = RequestMethod.POST)
+    public String postMasterData(MasterDataRequest masterDataRequest) {
+
+        int yearlyProduction = masterDataRequest.getYearlyCapacity();
+        int hourlyCapacity = masterDataRequest.getHourlyCapacity();
+
+        dataBean.setHourlyCapacity(hourlyCapacity);
+        dataBean.setYearlyProduction(yearlyProduction);
+
+
+        return "redirect:/marketing";
     }
 
 
--- src/main/java/de/adventureworks/produktionsplanung/controller/masterData/MasterDataRequest.java
+++ src/main/java/de/adventureworks/produktionsplanung/controller/masterData/MasterDataRequest.java
@@ -5,4 +5,23 @@ public class MasterDataRequest {
     private int hourlyCapacity;
 
     private int yearlyCapacity;
+
+    public MasterDataRequest() {
+    }
+
+    public int getHourlyCapacity() {
+        return hourlyCapacity;
+    }
+
+    public void setHourlyCapacity(int hourlyCapacity) {
+        this.hourlyCapacity = hourlyCapacity;
+    }
+
+    public int getYearlyCapacity() {
+        return yearlyCapacity;
+    }
+
+    public void setYearlyCapacity(int yearlyCapacity) {
+        this.yearlyCapacity = yearlyCapacity;
+    }
 }
