b372a31c6ceb45615f36e8888746b31eeb65bbc2
sercan <sercan.yeniguen@outlook.com>
2019-02-15 17:03:24 +0100

    ProductionSimulationUtil Methoden implementiert

--- src/main/java/de/adventureworks/produktionsplanung/model/services/productionTrial/ProductionSimulationUtil.java
+++ src/main/java/de/adventureworks/produktionsplanung/model/services/productionTrial/ProductionSimulationUtil.java
@@ -53,32 +53,94 @@ public final class ProductionSimulationUtil {
     }
 
     static Map<Component, Integer> addDeliveriesToWarehouseStock(Map<Component, Integer> warehouseStock, List<LogisticsObject> deliveries) {
-        return null;
+        for (LogisticsObject lo : deliveries) {
+            for (Component component : lo.getComponents().keySet()) {
+                warehouseStock.put(component, lo.getComponents().get(component));
+            }
+        }
+        return warehouseStock;
     }
 
     //gibt maximale mögliche Produktion zurück. Wenn dailyShouldProduction möglich ist, ist sie der Rückgabewert. Effektiv wird die actualProduction berechnet.
     static Map<Bike, Integer> tryToAchieveDailyProduction(Map<Bike, Integer> dailyShouldProduction, Map<Component, Integer> wareHouseStock, int maxProdCapacity) {
-        return null;
+        int cap = maxProdCapacity;
+        Map<Bike ,Integer> actuallyPossibleProduction = new HashMap<>();
+        for (Bike bike : dailyShouldProduction.keySet()) {
+            int achievdProductions = 1;
+            List<Component> neededComponents = bike.getComponents();
+            int neededAmount = dailyShouldProduction.get(bike);
+            for (int i = 0; i < neededAmount && cap > 0; i++) {
+                int forkAmount= wareHouseStock.get(neededComponents.get(0));
+                int frameAmount=wareHouseStock.get(neededComponents.get(1));
+                int saddleAmount= wareHouseStock.get(neededComponents.get(2));
+                if (forkAmount>0&&frameAmount>0&&saddleAmount>0) {
+                    wareHouseStock.put(neededComponents.get(0),forkAmount-1);
+                    wareHouseStock.put(neededComponents.get(1),frameAmount-1);
+                    wareHouseStock.put(neededComponents.get(2),saddleAmount-1);
+                    actuallyPossibleProduction.put(bike,achievdProductions++);
+                }
+            }
+        }
+        return actuallyPossibleProduction;
     }
 
     static int countBikes(Map<Bike, Integer> bikes) {
-        return 0;
+        int sum= 0;
+        for(Bike bike : bikes.keySet()){
+           sum+= bikes.get(bike);
+        }
+        return sum;
     }
 
 
     static Map<Component, Integer> substractProductionFromWarehouse(Map<Bike, Integer> production, Map<Component, Integer> warehouse) {
-        return null;
-
+        for(Bike bike: production.keySet()){
+            List<Component> bikeComponent = bike.getComponents();
+            for(Component component: bikeComponent){
+                int compStock= warehouse.get(component);
+                int subtractAmount= production.get(bike);
+                if(subtractAmount>compStock){
+                    throw new IllegalArgumentException("Du wolltest mehr löschen als überhaupt da war,... du Penner!");
+                }
+                warehouse.put(component,compStock-subtractAmount);
+            }
+        }
+        return warehouse;
     }
 
 
     static <T> Map<T, Integer> addMaps(Map<T, Integer> map1, Map<T, Integer> map2) {
-        return null;
+        Map<T,Integer> result= new HashMap<>();
+        for(T e: map1.keySet()){
+            if(map2.containsKey(e)){
+                result.put(e,map1.get(e)+map2.get(e));
+            }else{
+                result.put(e,map1.get(e));
+            }
+        }
+        for(T e: map2.keySet()){
+            if(!map1.containsKey(e)){
+                result.put(e,map1.get(e));
+            }
+        }
 
+        return result;
     }
 
-    static <T> Map<T, Integer> substractMaps(Map<T, Integer> minuend, Map<T, Integer> subtrahend) {
-        return null;
+    static <T> Map<T, Integer> substractMaps(Map<T, Integer> minuend, Map<T, Integer> subtrahend){
+        for(T e: subtrahend.keySet()){
+            if(!minuend.containsKey(e)){
+                throw new IllegalArgumentException("Die du willst ein Element aus der Map löschen was gar nicht vorhanden ist");
+            }
+            else if(subtrahend.get(e)>minuend.get(e)){
+                throw new IllegalArgumentException("Du möchtest mehr aus der Map löschen als vorhanden ist, das Problem ist bei"+e);
+            }
+            else{
+                int actualValue= minuend.get(e);
+                minuend.put(e,actualValue-subtrahend.get(e));
+            }
+        }
+        return minuend;
     }
 
 
