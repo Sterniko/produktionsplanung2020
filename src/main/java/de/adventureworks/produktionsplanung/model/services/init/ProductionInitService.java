f55927a8e55de029018b4714c6253cfbbacd52c0
robertkossendey <Gebaum11>
2019-02-16 12:30:14 +0100

    production init läuft am anfang

---
+++ src/main/java/de/adventureworks/produktionsplanung/model/services/init/ProductionInitService.java
@@ -0,0 +1,20 @@
+package de.adventureworks.produktionsplanung.model.services.init;
+
+import de.adventureworks.produktionsplanung.model.services.productionTrial.ProductionService2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+
+@Service
+public class ProductionInitService {
+
+    @Autowired
+    ProductionService2 productionService2;
+
+    @PostConstruct
+    public void initProductionStarter() {
+        productionService2.simulateInitialProduction(2019);
+    }
+}
