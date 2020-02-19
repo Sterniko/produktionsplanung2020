e052b9d0b8e968301661e1b3a13bc564c8b4842d
Kai Sternke <nope@gmx.de>
2020-02-19 15:48:34 +0100

    lager bearbeiten geht auch

--- src/main/java/de/adventureworks/produktionsplanung/controller/warehouse/WarehouseRequest.java
+++ src/main/java/de/adventureworks/produktionsplanung/controller/warehouse/WarehouseRequest.java
@@ -13,21 +13,62 @@ public class WarehouseRequest {
 
     private Map<String, Integer> warehouseMap;
 
-    public WarehouseRequest(LocalDate date, Map<String, Integer> warehouseMap) {
-        this.date = date;
+    public WarehouseRequest(String date, Map<String, Integer> warehouseMap) {
+        int ende = -1;
+        int anfang = -1;
+        ende = date.length();
+        anfang = date.length() - 2;
+        System.out.println("here");
+        int tag = Integer.parseInt(date.substring(anfang, ende));
+        ende = date.length() - 3;
+        anfang = date.length() - 5;
+        System.out.println("here");
+        int month = Integer.parseInt(date.substring(anfang, ende));
+        ende = date.length() - 6;
+        anfang = date.length() - 10;
+        System.out.println("here");
+        int jahr = Integer.parseInt(date.substring(anfang, ende));
+        this.date = LocalDate.of(jahr, month, tag);
         this.warehouseMap = warehouseMap;
     }
 
     public WarehouseRequest() {
-        warehouseMap= new HashMap<>();
+        warehouseMap = new HashMap<>();
     }
 
     public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
+    //public void setDate(LocalDate date) {
+    //   this.date = date;
+    //  }
+
+    public void setDate(String date) {
+        System.out.println(date);
+        int ende = -1;
+        int anfang = -1;
+
+        ende = date.length();
+        anfang = date.length() - 2;
+        String sub = date.substring(anfang, ende);
+        System.out.println(sub);
+        int tag = Integer.parseInt(sub);
+
+        ende = date.length() - 3;
+        anfang = date.length() - 5;
+        sub = date.substring(anfang, ende);
+        System.out.println(sub);//01
+        int month = Integer.parseInt(sub);
+
+        ende = date.length() - 6;
+        anfang = date.length() - 10;
+        sub = date.substring(anfang, ende);
+        System.out.println(sub);//1-
+        int jahr = Integer.parseInt(sub);
+
+
+        this.date = LocalDate.of(jahr, month, tag);
     }
 
     public Map<String, Integer> getWarehouseMap() {
@@ -39,8 +80,8 @@ public class WarehouseRequest {
     }
 
     public void setComponentAmout(String component, int amount) {
-      //  System.out.println(component);
-      //  System.out.println(amount);
+        //  System.out.println(component);
+        //  System.out.println(amount);
     }
 
     @Override
